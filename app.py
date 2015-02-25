24c11ef9e18ab1e6b2d166e56aea6505833ef27a
Omar Estrella <omarestrella@bitcreative.net>
2015-02-25 16:17:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+    
+@app.route('/mtg')
+def mtg():
+    return render_template('mtg.html')
 
 
 if __name__ == '__main__':
